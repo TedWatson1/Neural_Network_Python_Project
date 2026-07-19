b109ce7532e7d3693b2e9d2658b8aece10741be0
TedWatson1 <134866110+TedWatson1@users.noreply.github.com>
2026-07-19 19:34:30 +0100

    Update layer.py

--- layer.py
+++ layer.py
@@ -2,7 +2,6 @@ import numpy as np
 
 class Layer:
     def __init__(self, n_inputs, n_outputs, activation):
-        # 'relu', 'leakyrelu', 'linear', 'sigmoid', 'tanh', 'softplus', 'elu'
         match activation:
             case 'relu':
                 self.W = np.random.randn(n_inputs, n_outputs) * np.sqrt(2 / n_inputs)
