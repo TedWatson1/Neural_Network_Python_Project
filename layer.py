04739417bbfeac182554a0e450b9619f18165116
TedWatson1 <134866110+TedWatson1@users.noreply.github.com>
2026-07-21 12:55:19 +0100

    Update layer.py

--- layer.py
+++ layer.py
@@ -3,7 +3,6 @@ import numpy as np
 class Layer:
     def __init__(self, n_inputs, n_outputs, activation):
         match activation:
-        # Using the optimal standard deviations for each activation function
             case 'relu':
                 self.W = np.random.randn(n_inputs, n_outputs) * np.sqrt(2 / n_inputs)
             case 'leakyrelu':
